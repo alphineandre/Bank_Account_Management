bb831722df5b9fab39fdec091a8be9a2ca4cfe77
alphineandre <alphyandrechauke@gmail.com>
2024-11-20 13:19:55 +0200

    BankAccount class added

---
+++ src/bank/management/BankAccount.java
@@ -0,0 +1,36 @@
+package bank.management;
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount + ". New Balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds. Balance: " + balance);
+        }
+        balance -= amount;
+        System.out.println("Withdrawn: " + amount + ". Remaining Balance: " + balance);
+    }
+}
